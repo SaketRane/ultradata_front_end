738793b773a515f0d21e0f4df6b3e67b2bbfe437
max-tsymba <official.byte.story@gmail.com>
2025-07-08 13:44:35 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from '@/contexts/AuthContext';
 import Index from './pages/Index';
@@ -15,31 +15,27 @@ import PrivateGuard from './pages/guards/PrivateGuard';
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner position="top-right" expand={true} closeButton={true} />
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Navigate to={'/dashboard'} replace />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot-password" element={<ResetPassword />} />
-            <Route path="/reset-password" element={<UpdatePassword />} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateGuard>
-                  <Dashboard />
-                </PrivateGuard>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner position="top-right" expand={true} closeButton={true} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={'/dashboard'} replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ResetPassword />} />
+        <Route path="/reset-password" element={<UpdatePassword />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateGuard>
+              <Dashboard />
+            </PrivateGuard>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </TooltipProvider>
 );
 
 export default App;
