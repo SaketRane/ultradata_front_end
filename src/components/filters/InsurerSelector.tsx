c55b9ffd0597bc384124de71f50cb05abd9c6e8c
saketrane <saket.rane2005@gmail.com>
2025-09-10 18:48:36 -0400

    Hid the acquired and non-operational companies.

--- src/components/filters/InsurerSelector.tsx
+++ src/components/filters/InsurerSelector.tsx
@@ -21,10 +21,34 @@ interface InsurerSelectorProps {
 const InsurerSelector: React.FC<InsurerSelectorProps> = ({ insurer, setInsurer }) => {
   const [insurerSearchTerm, setInsurerSearchTerm] = useState<string>("");
   
+  // List of company codes to temporarily hide
+  const hiddenCompanyCodes = [
+    "A041", // Ascentus Insurance Ltd
+    "A095", // Canadian Direct Insurance Incorporated
+    "A254", // Coseco Insurance Company
+    "A258", // DAS Legal Protection Insurance Company Limited
+    "A460", // Guarantee Company of North America (The)
+    "A515", // ACE INA Insurance
+    "A530", // Kings Mutual Insurance Company (The)
+    "A560", // Northbridge Commercial Insurance Corporation
+    "A580", // Missisquoi Insurance Company (The)
+    "A660", // Perth Insurance Company
+    "A680", // Pictou County Farmers' Mutual Fire Insurance Company
+    "A870", // Waterloo Insurance Company
+    "D165", // Caisse Centrale de Reassurance
+    "D765", // T.H.E. Insurance Company
+    "D862", // Virginia Surety Company Inc.
+    "D871", // XL Insurance Company SE
+    "D416", // HDI Global Specialty SE
+  ];
+  
+  // Filter out hidden companies first, then apply search filter
+  const availableInsurers = insurers.filter(ins => !hiddenCompanyCodes.includes(ins.code));
+  
   const filteredInsurers = insurerSearchTerm.length > 0
-    ? insurers.filter(ins => 
+    ? availableInsurers.filter(ins => 
         ins.name.toLowerCase().startsWith(insurerSearchTerm.toLowerCase()))
-    : insurers;
+    : availableInsurers;
 
   const handleInsurerChange = (value: string) => {
     setInsurer(value);
